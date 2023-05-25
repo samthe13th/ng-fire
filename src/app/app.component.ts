14530f5c592ec2c023ec92bbdd380495e51e5ee0
Sam MacKinnon <sam.w.mackinnon@gmail.com>
2023-05-25 00:33:42 -0700

    update url params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,12 +22,20 @@ export class AppComponent implements OnInit {
   isKaraokeUser = false;
   karaokeUser = 0;
   stream = 'dolby';
-  admin = null;
   theme = 'karaoke';
-  room = "";
+  room = "bqfkn_show";
   showOpen = false;
   chatUrl: any;
   tvOverlay = true;
+  adminId = 'thebqfknadmin';
+  isAdmin = false;
+
+  karaokeIds: {[key: string]: number} = {
+    '2faf2Q3Jk': 0,
+    '93jfks9oF': 1,
+    '3jfkasdf3': 2
+  }
+
   karaokeUsers = [
     { name: 'Karaoke 1', url: '', vmixEmbed: false },
     { name: 'Karaoke 2', url: '', vmixEmbed: false },
@@ -43,18 +51,16 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.room = params?.['room'] ?? null;
-      this.admin = params?.['admin'] ?? null;
-      this.karaokeUser = toNumber(params?.['karaoke']) ?? null;
+      this.isAdmin = params?.['admin'] === this.adminId;
+      this.karaokeUser = this.getKaraokeUser(params?.['k']);
+      let chatParams = `?room=${this.room}&theme=${this.theme}`;
 
-      let chatParams = "";
-
-      if (this.room) {
-        chatParams = `?room=${this.room}&admin=${this.admin}&theme=${this.theme}`;
-        this.chatUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
-          "https://chatroom-d4ab9.web.app" + chatParams
-        );
+      if (this.isAdmin) {
+        chatParams += `&admin=${this.adminId}`
       }
+      this.chatUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+        "https://chatroom-d4ab9.web.app" + chatParams
+      );
       this.config$ = objectVal(this.getRef(['config'])).pipe(tap((config) => {
         if (config?.showOpen) {
           this.setStream('dolby');
@@ -62,6 +68,10 @@ export class AppComponent implements OnInit {
       }))
       this.setDatabaseProperties();
     })
+  }
+
+  getKaraokeUser(id: string) {
+    return this.karaokeIds[id];
   }
 
   onClipboardCopy(callback: any) {
